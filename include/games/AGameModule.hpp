97712dd5d7ef5aa5e84186ae1bff52261582efa7
Hugo ARNAL <hugo@hugoarnal.com>
2026-03-25 15:13:11 +0100

    feat (games): implement a very simple event queue

--- include/games/AGameModule.hpp
+++ include/games/AGameModule.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "cacarcade/IEvent.hpp"
 #include "cacarcade/IGameModule.hpp"
 #include "games/ScoreHandler.hpp"
+#include <queue>
 
 namespace arcade {
     class AGameModule : public cacarcade::IGameModule {
@@ -11,10 +13,13 @@ namespace arcade {
             ~AGameModule();
 
             cacarcade::TileContainer getTiles() const override;
+            std::optional<std::unique_ptr<cacarcade::IEvent>> pollEvent() override;
 
         protected:
             cacarcade::TileContainer _container;
             arcade::ScoreHandler _scoreHandler;
+
+            std::queue<std::unique_ptr<cacarcade::IEvent>> _eventQueue;
 
             virtual void handleEvent(std::unique_ptr<cacarcade::IEvent> &event);
             virtual void reset();
--- src/games/AGameModule.cpp
+++ src/games/AGameModule.cpp
@@ -1,4 +1,5 @@
 #include "games/AGameModule.hpp"
+#include "cacarcade/IEvent.hpp"
 
 arcade::AGameModule::AGameModule(const std::string name) : _container(), _scoreHandler(name)
 {
@@ -11,6 +12,17 @@ arcade::AGameModule::~AGameModule()
 cacarcade::TileContainer arcade::AGameModule::getTiles() const
 {
     return _container;
+}
+
+std::optional<std::unique_ptr<cacarcade::IEvent>> arcade::AGameModule::pollEvent()
+{
+    if (_eventQueue.empty())
+        return std::nullopt;
+
+    // TODO: I have no idea if this works at all
+    std::unique_ptr<cacarcade::IEvent> event = std::move(_eventQueue.front());
+    _eventQueue.pop();
+    return event;
 }
 
 void arcade::AGameModule::handleEvent(std::unique_ptr<cacarcade::IEvent> &)
