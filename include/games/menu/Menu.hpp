fe64e269a65ab65fa75810dcb6a9a4ae3b00c649
Hugo ARNAL <hugo@hugoarnal.com>
2026-04-13 11:06:25 +0200

    feat (menu): show highest score

--- include/games/menu/Menu.hpp
+++ include/games/menu/Menu.hpp
@@ -36,6 +36,7 @@ namespace arcade {
             void addDisplayContent();
             void addTitleContent();
             void addPlayersContent();
+            void addScoreContent();
             void clearLine(size_t line);
 
             void addLettersToPlayerName(std::unique_ptr<cacarcade::IEvent> &event);
